754e1f2eecda5112e9c3a7b23b2482e044230326
Ali Baizhanov <alibaizhanovbcc@gmail.com>
2018-08-14 09:28:58 +0600

    Selection sort

---
+++ SelecttionSort.py
@@ -0,0 +1,28 @@
+g = [4,45,6,9,1,3,7,8,21,19,20,21,86,5]
+
+
+
+def SelectSorted(arr):
+
+  smallest = arr[0]
+  smallectIndex = 0
+
+  for i in range(1,len(arr)):
+    if arr[i] < smallest:
+      smallest = arr[i]
+      smallectIndex = i
+
+  return smallectIndex
+
+
+def selectionSort(arr):
+  newArr = []
+
+  for i in range(len(arr)):
+    smallest = SelectSorted(arr)
+    newArr.append(arr.pop(smallest))
+
+  return newArr
+
+
+print(selectionSort(g))
