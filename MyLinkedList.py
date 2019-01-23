af45478c8c7758d369d3f06c58fa769372b05edd
Ali Baizhanov <alibaizhanovbcc@gmail.com>
2019-01-23 11:09:29 +0600

    F

---
+++ MyLinkedList.py
@@ -0,0 +1,13 @@
+
+class Node(object):
+    def __init__(self,val = None):
+        self.val = val
+        self.next = next
+
+
+class LinkedList(self,data):
+    new_node = Node(data)
+    cur = self.head
+
+
+
