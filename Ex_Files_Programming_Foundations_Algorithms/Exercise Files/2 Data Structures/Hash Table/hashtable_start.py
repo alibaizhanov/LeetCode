dc2d9e1c40c1dd187a57d81d151e105adc58ef35
Ali Baizhanov <alibaizhanovbcc@gmail.com>
2019-01-23 14:48:57 +0600

    New Work

---
+++ Ex_Files_Programming_Foundations_Algorithms/Exercise Files/2 Data Structures/Hash Table/hashtable_start.py
@@ -0,0 +1,26 @@
+# demonstrate hashtable usage
+
+
+# TODO: create a hashtable all at once
+
+item1 = dict({"key1":1,"key2":2, "key3":"three"})
+
+print(item1)
+# TODO: create a hashtable progressively
+
+item2 = {}
+item2["key1"] = 1
+item2["key2"] = 2
+item2["key3"] = 3
+
+print(item2)
+print(item1["key6"])
+
+# TODO: try to access a nonexistent key
+
+
+# TODO: replace an item
+
+
+
+# TODO: iterate the keys and values in the dictionary
